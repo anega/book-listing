7a007a8c66f94bb57c99564fa2db43466d94fa65
anega <anegamega@gmail.com>
2017-11-11 17:30:19 +0200

    Add 'no data', 'internet connection' errors handling; Add progress bar.

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.android.booklisting;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private static final String BOOK_URL = "https://www.googleapis.com/books/v1/volumes?q=android";
     // Constant value for the book loader ID
     private static final int BOOK_LOADER_ID = 0;
+    // TextView that is displayed when the list is empty
+    private TextView mEmptyStateTextView;
+    private ProgressBar mIndeterminateBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         // Create a new adapter that takes an empty list of earthquakes as input
         mAdapter = new BookAdapter(this, new ArrayList<Book>());
+        // Find a reference to the empty earthquakes view in the layout
+        mEmptyStateTextView = findViewById(R.id.tv_empty_books);
+        mIndeterminateBar = findViewById(R.id.indeterminateBar);
 
         // Set the adapter on the {@link} ListView, so the list can be populated in the user interface
         bookListView.setAdapter(mAdapter);
@@ -49,7 +60,23 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             }
         });
 
-        getLoaderManager().initLoader(BOOK_LOADER_ID, null, this);
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMngr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMngr.getActiveNetworkInfo();
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnectedOrConnecting()) {
+            getLoaderManager().initLoader(BOOK_LOADER_ID, null, this);
+        } else {
+            // Otherwise display error
+            // First hide loading indicator so error message will be wisible
+            mIndeterminateBar.setVisibility(View.GONE);
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_intenet);
+        }
+
+        // Set empty view for no books response
+        bookListView.setEmptyView(mEmptyStateTextView);
     }
 
     @Override
@@ -59,6 +86,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<List<Book>> loader, List<Book> books) {
+        // Hide progress bar when loading is finished
+        mIndeterminateBar.setVisibility(View.GONE);
+        // Set empty state text to display "No data available."
+        mEmptyStateTextView.setText(R.string.no_data_available);
         // Clear the adapter of previous earthquake data
         mAdapter.clear();
 
