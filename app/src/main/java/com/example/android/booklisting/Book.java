5b72f25789d0481a5e2f86be79269f5d2360142b
anega <anegamega@gmail.com>
2017-11-12 15:55:44 +0200

    Refactor, comment Book class

--- app/src/main/java/com/example/android/booklisting/Book.java
+++ app/src/main/java/com/example/android/booklisting/Book.java
@@ -4,7 +4,7 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 
 /**
- * Book class
+ * Book item class
  */
 public class Book {
     private String mTitle;
@@ -12,26 +12,53 @@ public class Book {
     private Bitmap mImgUrl;
     private String mBookUrl;
 
-    public Book(String title, String author, Bitmap imgUrl, String bookUrl) {
+    /**
+     * Book item constructor
+     *
+     * @param title    String book item title
+     * @param author   String book item author
+     * @param coverImg Bitmap book item cover image
+     * @param bookUrl  String book item details url
+     */
+    public Book(String title, String author, Bitmap coverImg, String bookUrl) {
         mTitle = title;
         mAuthor = author;
-        mImgUrl = imgUrl;
+        mImgUrl = coverImg;
         mBookUrl = bookUrl;
     }
 
-
+    /**
+     * Book title getter
+     *
+     * @return String title
+     */
     public String getTitle() {
         return mTitle;
     }
 
+    /**
+     * Book author getter
+     *
+     * @return String author
+     */
     public String getAuthor() {
         return mAuthor;
     }
 
+    /**
+     * Book image getter
+     *
+     * @return Bitmap image
+     */
     public Bitmap getCoverImg() {
         return mImgUrl;
     }
 
+    /**
+     * Book details url getter
+     *
+     * @return String url
+     */
     public String getBookUrl() {
         return mBookUrl;
     }
